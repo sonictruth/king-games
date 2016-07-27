c934f13bec7aa58f44bfb317d7d8f9d00a586c82
Alexandru Constantin <alex@vodafone.local>
2016-07-27 04:54:15 +0200

    clean up

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,8 +1,13 @@
+import jQuery from 'jquery';
+
 class HomeController {
   constructor(kingService, $scope) {
     'ngInject';
     this.$scope = $scope;
     this.$scope.carouselGames = kingService.getRandomGames();
+    this.$scope.$evalAsync(() => {
+      jQuery('.carousel').carousel({ interval: 3000 });
+    });
     this.name = 'home';
   }
 }
